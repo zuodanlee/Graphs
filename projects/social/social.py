4d016fcba84d6dd99d87768bbf475cf1688984af
zuodanlee <zuodanlee@gmail.com>
2021-06-19 01:38:19 +0800

    implemented remove_friendship() and added test script for TDD

--- projects/social/social.py
+++ projects/social/social.py
@@ -113,4 +113,3 @@ if __name__ == '__main__':
     connections = sg.get_all_social_paths(1)
     print(connections)
 
-#
---
+++ projects/social/test.py
@@ -0,0 +1,8 @@
+import pytest
+from social import *
+
+def test_remove_friendship():
+    sg = SocialGraph()
+    sg.populate_graph()
+    sg.remove_friendship(1, 8) # remove friendship between user1 and user8
+    assert 8 not in sg.friendships[1]
